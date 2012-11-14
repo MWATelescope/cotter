9c8ed7eb0558200485aff735cdb83842d1d30d1e
André Offringa <offringa@gmail.com>
2012-11-14 18:17:27 +1100

    whitespace

--- cotter/cotter.cpp
+++ cotter/cotter.cpp
@@ -13,9 +13,11 @@
 #include <map>
 #include <cmath>
 #include <complex>
+
 #include <xmmintrin.h>
 
 #define SPEED_OF_LIGHT 299792458.0        // speed of light in m/s
+
 #define USE_SSE
 
 using namespace aoflagger;
