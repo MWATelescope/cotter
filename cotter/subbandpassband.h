8d01873ccdc0313884f77fb1919277293389d929
André Offringa <offringa@gmail.com>
2014-04-11 10:28:32 +1000

    Important change to Cotter: enabling new passband coming from Levine's memo. Don't mix gain solutions from before and after this change.

--- cotter/subbandpassband.h
+++ cotter/subbandpassband.h
@@ -9,7 +9,13 @@ class SubbandPassband
 		static void GetSubbandPassband(std::vector<double> &passband, std::size_t channelsPerSubband);
 		
 	private:
-		const static double _sb128ChannelSubbandValue[128];
+		/**
+		 * See the cpp file for explanation of the difference between these bandpasses.
+		 */
+		const static double _sb128ChannelSubbandValue2013[128];
+		const static double _sb128ChannelSubbandValue2014FromMemo[128];
+		
+		const static double *_sb128ChannelSubbandValue;
 		
 		static double getSubbandValue(std::size_t channel, std::size_t channelCount);
 };
